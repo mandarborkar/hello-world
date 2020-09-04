4f3a3229af77f9ad50d46391e2c3aebe53475bd3
Mandar Borkar <mandar.borkar@gmail.com>
2020-09-04 16:08:17 -0700

    adding my git command file

---
+++ mygithubcommands.sh
@@ -0,0 +1,10 @@
+mkdir programming
+cd programming
+git clone https://github.com/mandarborkar/hello-world.git
+cd hello-world
+git status
+git add bubblesort.py
+git status
+git config user.email "mandar.borkar@gmail.com"
+git commit -m "added my first file"
+git push
