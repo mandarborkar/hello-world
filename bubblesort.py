72844cc1eb03e79f31656f90c028ce1cd6938e08
Mandar Borkar <mandar.borkar@gmail.com>
2020-09-04 12:37:16 -0700

    added my first file

---
+++ bubblesort.py
@@ -0,0 +1,11 @@
+inputarray = [1, 2, 3, 4, 5, 6]
+print ("My input array = ")
+print (inputarray)
+for i in range(len(inputarray)):
+    for j in range(len(inputarray)):
+        if inputarray[i]<inputarray[j]:
+            larger=inputarray[i]
+            inputarray[i]=inputarray[j]
+            inputarray[j]=larger
+print ("My output array = ")
+print (inputarray)
