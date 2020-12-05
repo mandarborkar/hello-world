4acbd9dadc4354340361bf6c8d58c54dbe4a0f63
mborkar <mandar.borkar@gmail.com>
2020-12-05 12:36:57 -0800

    Upload latest changes.

---
+++ .idea/avent5a1.py
@@ -0,0 +1,30 @@
+f1 = open("/Users/mborkar/PycharmProjects/hello-world/avent5input.txt", "r")
+boardingpass = f1.readlines()
+for i in range (0,len(boardingpass)):
+    boardingpass[i]=boardingpass[i].replace('\n','')
+print (boardingpass[0])
+
+seatbinary = []
+seatdecimalrow = []
+seatdecimalcolumn = []
+seatid = []
+for i in range (0,len(boardingpass)):
+    #print ('running pass='+ str(i))
+    seatbinary.append(boardingpass[i].replace("F","0").replace("B","1").replace("R","1").replace ("L","0"))
+    #print (boardingpass[i])
+    #print (seatbinary[i])
+    #print (seatbinary[i][:len(seatbinary[i])-3])
+    seatdecimalrow.append(int(seatbinary[i][:len(seatbinary[i])-3],2))
+    #print (seatdecimalrow[i])
+    #print (seatbinary[i][len(seatbinary[i])-3:])
+    seatdecimalcolumn.append(int(seatbinary[i][len(seatbinary[i])-3:],2))
+    #print (seatdecimalcolumn[i])
+    seatid.append (int((seatdecimalrow[i])*8)+int(seatdecimalcolumn[i]) )
+
+
+#print (seatbinary[0])
+#print (seatbinary[0][:int(len(seatbinary))-4])
+#print (seatdecimalrow[0])
+#print (seatdecimalcolumn[0])
+#print (seatid[0])
+print (max(seatid))
